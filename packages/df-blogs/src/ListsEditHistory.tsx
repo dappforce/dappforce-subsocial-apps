4650554c0362020df9fe9473111b5176a10f2ee8
OlehMell <oleh.melnechyk@gmail.com>
2019-08-01 11:04:18 +0300

    Update edit history modal for blog/post/comment

--- packages/df-blogs/src/ListsEditHistory.tsx
+++ packages/df-blogs/src/ListsEditHistory.tsx
@@ -1,18 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { withMulti, withCalls } from '@polkadot/ui-api/with';
 import { Modal, Comment as SuiComment, Button } from 'semantic-ui-react';
 import _ from 'lodash';
 import AddressMini from '@polkadot/ui-app/AddressMiniJoy';
-import { Post, Blog, PostId, PostData, BlogData, BlogId, CommentId, CommentData, Comment, OptionComment } from './types';
+import { Post, Blog, PostId, PostData, BlogData, BlogId, CommentId, CommentData, Comment, OptionComment, BlogHistoryRecord, CommentHistoryRecord, PostHistoryRecord } from './types';
 import { queryBlogsToProp, getJsonFromIpfs } from './utils';
 import { Option } from '@polkadot/types';
 import ReactMarkdown from 'react-markdown';
 import IdentityIcon from '@polkadot/ui-identicon/Identicon';
 import { Link } from 'react-router-dom';
+import { CreatedBy } from './CreatedBy';
 
 type ModalController = {
   open: boolean,
   close: () => void
+};
+
+type PropsCommentFromHistory = {
+  history: CommentHistoryRecord
+};
+
+const CommentFromHistory = (props: PropsCommentFromHistory) => {
+
+  const { history: { old_data, edited } } = props;
+  const { ipfs_hash } = old_data;
+  const [ content, setContent ] = useState({} as CommentData);
+
+  useEffect(() => {
+    getJsonFromIpfs<CommentData>(ipfs_hash.toString()).then(data => setContent(data)).catch(err => new Error(err));
+  });
+
+  return (<div style={{ textAlign: 'left', margin: '1rem' }}>
+        <SuiComment>
+          <SuiComment.Metadata>
+                <AddressMini
+                  value={edited.account}
+                  isShort={true}
+                  isPadded={false}
+                  size={28}
+                  extraDetails={`${edited.time.toLocaleString()} at block #${edited.block.toNumber()}`}
+                />
+          </SuiComment.Metadata>
+          <SuiComment.Text>{content.body}</SuiComment.Text>
+        </SuiComment>
+        <hr></hr>
+      </div>);
 };
 
 type CommentHistoryProps = ModalController & {
@@ -33,28 +65,7 @@ const InnerCommentHistoryModal = (props: CommentHistoryProps) => {
   console.log(edit_history);
 
   const renderCommentHistory = () => {
-
-    return edit_history.map((x, index) => {
-      const [ content, setContent ] = useState({} as CommentData);
-      const ipfsHash = x.old_data.ipfs_hash.toString();
-      getJsonFromIpfs<CommentData>(ipfsHash).then(data => setContent(data)).catch(err => new Error(err));
-      console.log(content);
-      return (<div key={index} style={{ textAlign: 'left', margin: '1rem' }}>
-        <SuiComment>
-          <SuiComment.Metadata>
-                <AddressMini
-                  value={x.edited.account}
-                  isShort={true}
-                  isPadded={false}
-                  size={28}
-                  extraDetails={`${x.edited.time.toLocaleString()} at block #${x.edited.block.toNumber()}`}
-                />
-          </SuiComment.Metadata>
-          <SuiComment.Text>{content.body}</SuiComment.Text>
-        </SuiComment>
-        <hr></hr>
-      </div>);
-    });
+    return edit_history.map((x,index) => <CommentFromHistory history={x} key={index} />);
   };
 
   return (
@@ -82,6 +93,36 @@ export const CommentHistoryModal = withMulti(
   )
 );
 
+type PropsPostFromHistory = {
+  history: PostHistoryRecord
+};
+
+const PostFromHistory = (props: PropsPostFromHistory) => {
+
+  const { history: { old_data, edited } } = props;
+  const { ipfs_hash } = old_data;
+  const [ content, setContent ] = useState({} as PostData);
+
+  useEffect(() => {
+    if (ipfs_hash.isNone) return;
+    const ipfsHash = ipfs_hash.unwrap().toString();
+    getJsonFromIpfs<PostData>(ipfsHash).then(data => setContent(data)).catch(err => new Error(err));
+  });
+
+  return (<div style={{ textAlign: 'left', margin: '1rem' }}>
+    <h1 style={{ display: 'flex' }}>
+      <span style={{ marginRight: '.5rem' }}>{content.title}</span>
+    </h1>
+    <CreatedBy created={edited} />
+    <div style={{ margin: '1rem 0' }}>
+      {content.image && <img src={content.image} className='DfPostImage' /* add onError handler */ />}
+      <ReactMarkdown className='JoyMemo--full' source={content.body} linkTarget='_blank' />
+      {/* TODO render tags */}
+    </div>
+    <hr></hr>
+  </div>);
+};
+
 type PostHistoryProps = ModalController & {
   id: PostId,
   postOpt?: Option<Post>
@@ -99,26 +140,7 @@ const InnerPostHistoryModal = (props: PostHistoryProps) => {
   console.log(edit_history);
 
   const renderPostHistory = () => {
-
-    return edit_history.map((x, index) => {
-      const [ content, setContent ] = useState({} as PostData);
-      const ipfsHashOpt = x.old_data.ipfs_hash;
-      if (ipfsHashOpt.isNone) return <></>;
-      const ipfsHash = ipfsHashOpt.unwrap().toString();
-      getJsonFromIpfs<PostData>(ipfsHash).then(data => setContent(data)).catch(err => new Error(err));
-      console.log('Content #' + index + [ content ]);
-      return (<div key={index} style={{ textAlign: 'left', margin: '1rem' }}><h1 style={{ display: 'flex' }}>
-        <span style={{ marginRight: '.5rem' }}>{content.title}</span>
-      </h1>
-      {/* <CreatedBy edited={x.edited} /> */}
-      <div style={{ margin: '1rem 0' }}>
-        {content.image && <img src={content.image} className='DfPostImage' /* add onError handler */ />}
-        <ReactMarkdown className='JoyMemo--full' source={content.body} linkTarget='_blank' />
-        {/* TODO render tags */}
-      </div>
-      <hr></hr>
-    </div>);
-    });
+    return post.edit_history.map((x,index) => <PostFromHistory history={x} key={index} />);
   };
 
   return (
@@ -151,6 +173,44 @@ type BlogHistoryProps = ModalController & {
   blogOpt?: Option<Blog>
 };
 
+type PropsBlogFromHistory = {
+  history: BlogHistoryRecord
+};
+
+const BlogFromHistory = (props: PropsBlogFromHistory) => {
+
+  const { history: { old_data, edited } } = props;
+  const { ipfs_hash } = old_data;
+  const [ content, setContent ] = useState({} as BlogData);
+
+  useEffect(() => {
+    if (ipfs_hash.isNone) return;
+    const ipfsHash = ipfs_hash.unwrap().toString();
+    getJsonFromIpfs<BlogData>(ipfsHash).then(data => setContent(data)).catch(err => new Error(err));
+  });
+
+  return (<div style={{ textAlign: 'left', margin: '1rem' }}>
+      <div className='ui massive relaxed middle aligned list FullProfile'>
+        <div className={`item ProfileDetails MyProfile`}>
+        {content.image
+        ? <img className='ui avatar image' src={content.image} />
+        : <IdentityIcon className='image' value={edited.account} size={40} />
+        }
+          <div className='content'>
+            <div className='header'>
+              <Link to='' className='handle'>{content.name}</Link>
+            </div>
+            <div className='description' style={{ margin: '0.2rem' }}>
+              <ReactMarkdown className='JoyMemo--full' source={content.desc} linkTarget='_blank' />
+            </div>
+          </div>
+        </div>
+      </div>
+      <CreatedBy created={edited} />
+      <hr/>
+  </div>);
+};
+
 const InnerBlogHistoryModal = (props: BlogHistoryProps) => {
 
   const { open, close, blogOpt } = props;
@@ -159,38 +219,9 @@ const InnerBlogHistoryModal = (props: BlogHistoryProps) => {
   else if (blogOpt.isNone) return <Modal>Blog not found</Modal>;
 
   const blog = blogOpt.unwrap();
-  const { edit_history } = blog;
-  console.log(edit_history);
 
   const renderBlogHistory = () => {
-
-    return edit_history.map((x, index) => {
-      const [ content, setContent ] = useState({} as BlogData);
-      const ipfsHashOpt = x.old_data.ipfs_hash;
-      if (ipfsHashOpt.isNone) return <></>;
-      const ipfsHash = ipfsHashOpt.unwrap().toString();
-      getJsonFromIpfs<BlogData>(ipfsHash).then(data => setContent(data)).catch(err => new Error(err));
-      console.log(content);
-      return (<div key={index} style={{ textAlign: 'left', margin: '1rem' }}>
-        <div className='ui massive relaxed middle aligned list FullProfile'>
-          <div className={`item ProfileDetails MyProfile`}>
-          {content.image
-          ? <img className='ui avatar image' src={content.image} />
-          : <IdentityIcon className='image' value={x.edited.account} size={40} />
-          }
-            <div className='content'>
-              <div className='header'>
-                <Link to={`/blogs/${blog.id}`} className='handle'>{content.name}</Link>
-              </div>
-              <div className='description'>
-                <ReactMarkdown className='JoyMemo--full' source={content.desc} linkTarget='_blank' />
-              </div>
-            </div>
-          </div>
-        </div>
-      <hr></hr>
-    </div>);
-    });
+    return blog.edit_history.map((x,index) => <BlogFromHistory history={x} key={index} />);
   };
 
   return (
@@ -202,7 +233,7 @@ const InnerBlogHistoryModal = (props: BlogHistoryProps) => {
     >
       <Modal.Header><h1>Edit History</h1></Modal.Header>
       <Modal.Content scrolling>
-        {edit_history && renderBlogHistory()}
+        {history && renderBlogHistory()}
       </Modal.Content>
       <Modal.Actions>
         <Button content='Close' onClick={close} />
